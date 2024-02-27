b029974f89293af69e41dfcaa7f6e099215bbe28
demo_repo <dinmuhammadrajpar@gmail.com>
2024-02-27 20:38:21 +0500

    question 4 added

---
+++ question_4.ts
@@ -0,0 +1,12 @@
+let quote:string = " \"Success is not final, failure is not fatal: It is the courage to continue that counts.\"";
+let author:string = "winston churchill";
+
+function toTitleCase(str: string): string {
+    return str.toLowerCase().replace(/(?:^|\s)\w/g, function(match) {
+        return match.toUpperCase();
+    });
+}
+
+const author_name = toTitleCase(author);
+
+console.log(author_name + " once said, " + quote);
