bbf9a6beb1000d4238dc26944071463d30eb475b
demo_repo <dinmuhammadrajpar@gmail.com>
2024-03-01 16:32:14 +0500

    question 6 added

---
+++ question_6.js
@@ -0,0 +1,4 @@
+var personName = "DinMuhammadRajpar";
+var changed_personName = " Din\tMuhammad\nRajpar";
+console.log(personName);
+console.log(changed_personName);
---
+++ question_6.ts
@@ -0,0 +1,6 @@
+let personName:string = "DinMuhammadRajpar";
+
+let changed_personName:string = " Din\tMuhammad\nRajpar";
+
+console.log(personName);
+console.log(changed_personName);
